bced31dcd1103bcc71cfd827d0dd50b4caf46fe4
Francisco Zapata <drfcozapata@gmail.com>
2023-08-10 18:44:01 -0400

    Renderizado de proyectos a partir de JSON

--- src/components/home/Projects.vue
+++ src/components/home/Projects.vue
@@ -30,136 +30,19 @@
 		<article
 			class="md:grid md:grid-cols-2 lg:grid-cols-3 md:grid-rows-4 lg:grid-rows-3 md:gap-10 lg:w-3/4 mx-auto px-10 md:px-20 lg:px-0 mb-10"
 		>
-			<RouterLink to="/project/1">
+			<RouterLink
+				v-for="{ id, name, image, description } in projects"
+				:key="id"
+				:to="`/project/${id}`"
+			>
 				<div class="card card-compact w-full bg-base-100 shadow-xl mb-10 md:mb-0">
 					<figure class="h-[200px] image">
-						<img src="/project-01.png" alt="" />
+						<img :src="image" alt="" />
 					</figure>
 					<div class="card-body">
-						<h2 class="card-title">Guadalajara Web Camp</h2>
+						<h2 class="card-title">{{ name }}</h2>
 						<p>
-							My first personal project, part of the Udemy course "Desarrollo Web
-							Completo" I took in 2020.
-						</p>
-					</div>
-				</div>
-			</RouterLink>
-			<RouterLink to="/project/2">
-				<div class="card card-compact w-full bg-base-100 shadow-xl mb-10 md:mb-0">
-					<figure class="h-[200px] image">
-						<img src="/project-02.png" alt="" />
-					</figure>
-					<div class="card-body">
-						<h2 class="card-title">Rock & EDM Festival</h2>
-						<p>
-							My second personal project, part of the Udemy course "Desarrollo Web
-							Completo" I took in 2020.
-						</p>
-					</div>
-				</div>
-			</RouterLink>
-			<RouterLink to="/project/3">
-				<div class="card card-compact w-full bg-base-100 shadow-xl mb-10 md:mb-0">
-					<figure class="h-[200px] image">
-						<img src="/project-03.png" alt="" />
-					</figure>
-					<div class="card-body">
-						<h2 class="card-title">Blog de Café</h2>
-						<p>
-							My third personal project, part of the Udemy course "Desarrollo Web
-							Completo" I took in 2020.
-						</p>
-					</div>
-				</div>
-			</RouterLink>
-			<RouterLink to="/project/4">
-				<div class="card card-compact w-full bg-base-100 shadow-xl mb-10 md:mb-0">
-					<figure class="h-[200px] image">
-						<img src="/project-04.png" alt="" />
-					</figure>
-					<div class="card-body">
-						<h2 class="card-title">Portafolio 2021</h2>
-						<p>
-							My first portfolio, created as a requirement of my studies at Academlo
-							(2021).
-						</p>
-					</div>
-				</div>
-			</RouterLink>
-			<RouterLink to="/project/5">
-				<div class="card card-compact w-full bg-base-100 shadow-xl mb-10 md:mb-0">
-					<figure class="h-[200px] image">
-						<img src="/project-05.png" alt="" />
-					</figure>
-					<div class="card-body">
-						<h2 class="card-title">I Love My Yacht</h2>
-						<p>Website of "I Love My Yacht", a company located in Florida, USA (2022).</p>
-					</div>
-				</div>
-			</RouterLink>
-			<RouterLink to="/project/6">
-				<div class="card card-compact w-full bg-base-100 shadow-xl mb-10 md:mb-0">
-					<figure class="h-[200px] image">
-						<img src="/project-06.png" alt="" />
-					</figure>
-					<div class="card-body">
-						<h2 class="card-title">Iglesia Bautista Fe</h2>
-						<p>Website of Faith Baptist Church located in Fortworth, TX, USA (2022).</p>
-					</div>
-				</div>
-			</RouterLink>
-			<RouterLink to="/project/7">
-				<div class="card card-compact w-full bg-base-100 shadow-xl mb-10 md:mb-0">
-					<figure class="h-[200px] image">
-						<img src="/project-07.png" alt="" />
-					</figure>
-					<div class="card-body">
-						<h2 class="card-title">EwexCar</h2>
-						<p>
-							I was part of the UPH&Blue Group team that developed the frontend of the
-							EwexCar website.
-						</p>
-					</div>
-				</div>
-			</RouterLink>
-			<RouterLink to="/project/8">
-				<div class="card card-compact w-full bg-base-100 shadow-xl mb-10 md:mb-0">
-					<figure class="h-[200px] image">
-						<img src="/project-08.png" alt="" />
-					</figure>
-					<div class="card-body">
-						<h2 class="card-title">Vue 3 New Documentation</h2>
-						<p>
-							I coordinated the team translating the new Vue 3 documentation into Spanish
-							(2023).
-						</p>
-					</div>
-				</div>
-			</RouterLink>
-			<RouterLink to="/project/9">
-				<div class="card card-compact w-full bg-base-100 shadow-xl mb-10 md:mb-0">
-					<figure class="h-[200px] image">
-						<img src="/project-09.png" alt="" />
-					</figure>
-					<div class="card-body">
-						<h2 class="card-title">My Portfolio 2023</h2>
-						<p>
-							My personal portfolio in 2023 (this very site), updated and made entirely
-							using Vue.js 3 and its ecosystem.
-						</p>
-					</div>
-				</div>
-			</RouterLink>
-			<RouterLink to="/project/10">
-				<div class="card card-compact w-full bg-base-100 shadow-xl mb-10 md:mb-0">
-					<figure class="h-[200px] image">
-						<img src="/project-10.png" alt="" />
-					</figure>
-					<div class="card-body">
-						<h2 class="card-title">Packing List Generator</h2>
-						<p>
-							Project developed for the logistics company Miami Carga to generate Packing
-							(2023).
+							{{ description[0] }}
 						</p>
 					</div>
 				</div>
@@ -168,7 +51,9 @@
 	</section>
 </template>
 
-<script setup></script>
+<script setup>
+	import { projects } from '../../data/projects.json';
+</script>
 
 <style lang="scss" scoped>
 	.projects {
@@ -176,7 +61,7 @@
 		background-image: url(../../assets/img/bg-stripes.svg);
 		background-position: center center;
 		background-repeat: no-repeat;
-		background-size: 70vh;
+		background-size: 90%;
 		padding: 80px 0;
 		clip-path: polygon(0 0, 100% 100px, 100% 100%, 0 calc(100% - 80px));
 
